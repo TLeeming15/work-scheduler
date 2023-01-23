bd5db754497e7222006cd4ddfb16236fad3d6a1c
TLeeming15 <tyler.leeming21@gmail.com>
2023-01-23 10:41:40 +1030

    fixed mixed up past future

--- Assets/script.js
+++ Assets/script.js
@@ -1,77 +1,28 @@
+// Displays current day
+var dateTime = dayjs().format('dddd, MMMM, DD, YYYY - H')
+var dateTimePieces = dateTime.split(" - ")
 
-let currentDate = $("#currentDay");
- 
+console.log(dateTimePieces)
+console.log(element)
 
-// universal variables
-let blockContainer = $ ("#block-container")
-let timeBlockClasses = ["row", "time-block"];
-let timeClasses = ["col-2", "col-md-1", "hour", "text-center", "py-3" ];
-let textAreaClasses = ["col-8", "col-md-10", "desctiption"];
-let saceBtnClasses = ["btn", "saveBtn", "col-2", "col-md-1"];
-let saveImageClasses = ["fas", "fa-save"];
-let pastClass = "past";
-let presentClass = "present";
-let futureClasss = "future";
+var hour = dateTimePieces[1]
+hour = parseInt(hour)
+$("#currentDay").text(dateTimePieces[0])
 
-const visibleRows = 3;
+var timeBlocks = $(".time-block").toArray()
+console.log(timeBlocks)
 
+for(let i = 0; i<timeBlocks.length; i++) {
+  var timeBlockHour = i+9
+  var element = timeBlocks[i];
 
-
-const workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
- const dataIndex = "data-index";
-
-
-// Adds blocks
-
-$(function () {
-
-  addBlocks();
-
-  let blockStorage = retrieveStorage
-
-  renderBlocks (blockStorage);
- 
-});
-// renders blocks
-function renderBlocks(blockStorage) {
-
- 
-  for (let i = 0; i < schStorage.length; i++) {
-
-    blockContainer.children().eq(blockStorage[i].id).children("textarea").val(blockStorage[i].schedule);    
+  console.log(hour);
+  console.log(timeBlockHour)
+  if (hour > timeBlockHour) {
+    element.classList.add("past")
+  } else if (hour===timeBlockHour) {
+    element.classList.add("present")
+  } else if (hour<timeBlockHour) {
+    element.classList.add("future")
   }
 }
-
-
-// function addBlocks ();
-
-//   for (let i=); i < workHours.length; i++){
-//     let wHour = workHours [i];
-
-//     let hour = (wHour);
-
-//     let className = getClassName (hour);
-
-//     let blockDiv = $("<div>");
-//     blockDiv.addClass(className)
-
-//     let time = $("<div>");
-//     time.text(wHour);
-//     time.addClass(timeClasses.join(""))
-
-//     let textArea = $ ("<textarea>");
-//     textArea.addClass(textAreaClasses.join(""));
-//     textArea.attr("rows", visibleRows);
-//   }
-
-//  retrieves current hour
-  var currentHour = dayjs().hour()
-  $(".time-block").each(function(){
-    var timeBlock = $(this)
-    console.log(this.id)
-    timeBlock.addClass("present")
-  })
-
-// Displays current day
-$("#currentDay").text(dayjs().format('dddd, MMMM, DD, YYYY'))
-
